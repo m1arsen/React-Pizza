c83404c44b694eb79bf790232366942c550f40bd
m1arsen <m1arsen.web@gmail.com>
2024-09-24 21:12:41 +0300

    feat: adding database with pizzas and logic to pizza card

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,25 +1,38 @@
 import { useState } from 'react';
 
-const PizzaBlock = ({ title, price }) => {
+const PizzaBlock = ({ title, price, imageUrl, types, sizes }) => {
   const [count, setCount] = useState(0);
+  const [activeType, setActiveType] = useState(0);
+  const [activeSize, setActiveSize] = useState(0);
+
+  const typeNames = ['тонкое', 'традиционное'];
 
   return (
     <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-      />
+      <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {types.map((typeId, i) => (
+            <li
+              key={i}
+              className={i === activeType ? 'active' : ''}
+              onClick={() => setActiveType(i)}
+            >
+              {typeNames[typeId]}
+            </li>
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((size, i) => (
+            <li
+              key={i}
+              className={i === activeSize ? 'active' : ''}
+              onClick={() => setActiveSize(i)}
+            >
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
