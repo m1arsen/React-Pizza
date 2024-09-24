7dfffe745f7f479d1de75a8dd526eda21f485f88
m1arsen <m1arsen.web@gmail.com>
2024-09-24 19:31:15 +0300

    feat: adding to PizzaBlock counter

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
   return (
     <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
             <PizzaBlock title="Мексиканская" price="500" />
           </div>
         </div>
