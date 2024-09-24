837d81ee61244f6a36b0b7f492f92662e6689997
m1arsen <m1arsen.web@gmail.com>
2024-09-24 18:51:55 +0300

    feat: adding new components - Header, Categories, Sort, PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
 import './scss/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <p>React Pizza</p>
+    <div className="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price="500" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
