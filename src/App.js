d3aac7dcca2661e4612d3f08c079e7f2e9d6834d
m1arsen <m1arsen.web@gmail.com>
2024-09-25 00:00:07 +0300

    feat: migrating the database pizza.json in mockAPI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState, useEffect } from 'react';
+
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
-
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const apiBase = process.env.REACT_APP_MOCKAPI_TOKEN;
+
+  useEffect(() => {
+    fetch(`https://${apiBase}.mockapi.io/items`)
+      .then((res) => res.json())
+      .then((json) => setItems(json));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -19,7 +29,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((props) => (
+            {items.map((props) => (
               <PizzaBlock key={props.id} {...props} />
             ))}
           </div>
