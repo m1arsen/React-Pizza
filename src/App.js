69c6705653d82c149aac40ea7a4cb570c8711d25
m1arsen <m1arsen.web@gmail.com>
2024-09-25 15:34:31 +0300

    feat: adding react router and pages - Cart, Home, NotFound

--- src/App.js
+++ src/App.js
@@ -1,44 +1,25 @@
-import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
 
-import Skeleton from './components/PizzaBlock/Skeleton';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const apiBase = process.env.REACT_APP_MOCKAPI_TOKEN;
-
-  useEffect(() => {
-    fetch(`https://${apiBase}.mockapi.io/items`)
-      .then((res) => res.json())
-      .then((json) => {
-        setItems(json);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-              : items.map((props) => <PizzaBlock key={props.id} {...props} />)}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+            <Route />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const apiBase = process.env.REACT_APP_MOCKAPI_TOKEN;
+
+  useEffect(() => {
+    fetch(`https://${apiBase}.mockapi.io/items`)
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((props) => <PizzaBlock key={props.id} {...props} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;
