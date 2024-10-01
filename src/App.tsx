cfc25939bf7eacf464bda2780d0dba37cd291a7e
m1arsen <m1arsen.web@gmail.com>
2024-10-01 19:18:49 +0300

    refactor: adding lazy loading of the Cart and FullPizza pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
 
 import Home from './pages/Home';
-import Cart from './pages/Cart';
-import FullPizza from './pages/FullPizza';
 import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
+
+const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
+const FullPizza = lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="pizza/:pizzaId" element={<FullPizza />} />
+        <Route
+          path="cart"
+          element={
+            <Suspense fallback={<div>Идет загрузка корзины...</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="pizza/:pizzaId"
+          element={
+            <Suspense fallback={<div>Идет загрузка пиццы...</div>}>
+              <FullPizza />
+            </Suspense>
+          }
+        />
         <Route path="*" element={<NotFound />} />
         <Route />
       </Route>
