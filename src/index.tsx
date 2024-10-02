70617531689a086909a9d34b73a3535a668020d1
m1arsen <m1arsen.web@gmail.com>
2024-10-02 22:34:21 +0300

    build: setting up a project to publish on github pages

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="React-Pizza">
     <Provider store={store}>
       <App />
     </Provider>
